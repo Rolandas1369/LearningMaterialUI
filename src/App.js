5a887eaa3f4dc133e1e8b1be57ab40d45c394fde
Rolandas1369 <rolandaswb@gmail.com>
2022-10-16 12:27:25 +0300

    install add button

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" onClick={() => console.log('click event')}>First ui comp</Button>
+    </div>
+  );
+}
+
+export default App;
